c4d276fbb9e95606053c0f008f6637705f1a519c
nthomas <n.thomas@robotikdose.com>
2016-09-15 10:03:05 +0200

    multiple asar

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
 # electron-asar-modularupdater
-Check update for electron modular asar files (with zip support included)
+> A NodeJs module for Electron, that handles multiple modular asar updates (with zip support included).
+
+## How it works (Read this first)
+* EAU (Electron Basic Updater) was built to handle the process of updating an Electron app in the most basic way; **it simply replaces the application files (at /resources/app/) with the new ones representing the "update"!** 
+    f this is what you are looking for then I hope you like it, else please check out a more advanced tool to handle this, somthing like [electron-updater](https://www.npmjs.com/package/electron-updater). Or have a look at [this](http://electron.atom.io/docs/v0.33.0/api/auto-updater/).
+* The check for "updates" must by triggered by the application. **EAU doesn't make any kind of periodic checks on its own**. You can use  something like [node-schedule](https://www.npmjs.com/package/node-schedule) for this task.
+* EAU talks to an API (let's call it so) to tell it if there is a new update.
+    * The API recieves a request from EAU with the client's **current version of the application (must be specified inside the application package.json file)**.
+    * The API then responds with the new update, ... or simply *false* to abort.
+    * If there's an update available the API should respond with the *source* for this update **.zip** file.
+    * EAU then downloads the .zip file and - I am sorry for the following - extracts it directly to the application folder (/resources/app/), thus overwriting the current files, including itself!
+
+## But why ? (use cases)
+Well, right now I am working on an Electron application for me and my collegues to use at work, after finishing the application and giving them the .exe release I needed to change couple of things, so I asked myself: what to do now ?! I read [this](http://electron.atom.io/docs/v0.33.0/api/auto-updater/) but for the life of me I couldn't get it to work :( (but to be fair, I didn't give it enough time, I wanted to create this instead :) ). So I decided to create a module to handle this, in the most basic way possible. So ...
+* You can use EAU when you are still building the application and have a group of people live-testing it on their machines, with you monitoring the process and pushing changes.
+* Also if your application is a local thing, inside your company or work group, and you simply need to push "changes" to their copies.
+* If you don't know how to get the [Squirrel](https://github.com/Squirrel) thing to work and need to roll out your application asap.
+
+
+---
+
+## Installation
+```
+    $ npm install --save electron-asar-modularupdater
+```
+Now, inside the *main.js* file, call it like this:
+```
+    const Electron = require('electron');
+    const Application = Electron.app;
+    const EAU = require('electron-asar-modularupdater');
+    
+    Application.on('ready', function(){
+        // Initiate the module
+        EAU.init({
+            'api': 'http:// .... ' // The API EAU will talk to
+        });
+    });
+```
+
+That's it. Now, you can use ```EAU.check()``` to trigger the update process; EAU will first check for updates, if there was a new update, EAU will download it and extract it to the application folder. Inside a window you can use it like this:
+
+```
+    <script>
+        var remote = require('remote'),
+            app = remote.require('app'),
+            EAU = remote.require('electron-basic-updater');
+            
+        function update(){
+            EAU.check(function(error){
+                if(error){
+                    alert(error);
+                    return false;
+                }
+                
+                alert('App updated successfully! Restart it please.');
+                app.quit();
+            });
+        }
+    </script>
+    
+    <a href="#" onclick="update()"> Update! </a>
+```
+
+---
+
+# API
+
+### `Init( setup )`
+
+* **setup** (object) The module setup
+    * **api** (string) The URL EAU will call to check for updates.
+    * **logFile** (string) [optional] The file to log the update process updates and errors to it, pass *FALSE* to omit logging . Defaults to "updater-log.txt".
+    * **requestOptions** (object) [optional] The global options for the HTTP requests EAU will make to check for updates and download them. EAU uses the cool [restler](https://github.com/danwrong/restler) for these requests, and the `requestOptions` will be used for all the requests ([the full options list](https://github.com/danwrong/restler#options)). You can use this option to pass an `accessToken` or a `username` and `password` along with the request, or even send some extra data along with the request through the `data` property.
+    
+        ```
+            EAU.init({
+                'api': 'http:// ...',
+                'requestOptions': {'accessToken': ..., 'data': {'group': 'friends'}}
+            });    
+        ```
+    * **callback** (function) [optional] The callback that will be trigger at the end of the process, whether the update was a success or not. You can set the callback here, or you can pass it directly to `check( ... )`, I use the later option, to be able to `console.log()` the error in the DevTools.
+    
+    ```
+        EAU.init({
+            'callback': function(error){ ... }
+        });
+    ```
+
+### `check( callback )`
+
+Will check for an update, if an update was found, will download it and install it! As mentioned, this method must be tirggerd, EAU wont check for updates on its own.
+* **callback** The update result callback. Check the installation section above for more details.
+
+---
+
+## The update server
+And I mean this in the most simple way possible. This server/API will recieve one request, which is the check for updates, and will send one response of :
+
+* **New update:** `{"last": " [the new version] ", "source": " [the .zip file url] "}` **EAU wont make any version comparsions, it will simply download the `source` url and extract it**. So, you will need to handle this on your side, EAU sends (POST-type request) you the client's current version (as `current`), you can use this to send the correct update!
+* **Any other value, to cancel the update**
+
+My current *update server* (for the app I descriped above) is:
+```
+    <?php
+        print json_encode([
+            'last' => '1.0.1',
+            'source' => 'http:// ... /update.zip'
+        ]);
+```
+
+I change this manually and tell the guys to hit the "update" button, which will trigger `.check()`
+
+---
+
+* Please contact me with any comments or open an issue.
+* The development of this module will be continued.
+
+---
+
+The MIT License (MIT) - 
+Copyright (c) 2016 Nicolas Thomas thedeadofblackfire@yahoo.com
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require('fs')
-//const _ = require('lodash')
 const path = require('path')
 const got = require('got')
 const fileSystem = require('original-fs');
 const Zip = require('adm-zip');
-//const os = require('os')
+const spawn = require('child_process').spawn;
 
 const errors = [
     'version_not_specified',
@@ -68,6 +67,19 @@ const _getFilename = function (str) {
     return str.split('\\').pop().split('/').pop();
 }
 
+const _getAsUriParameters = function (data) {
+  return Object.keys(data).map(function (k) {
+    if (Array.isArray(data[k])) {
+      var keyE = encodeURIComponent(k + '[]');
+      return data[k].map(function (subData) {
+        return keyE + '=' + encodeURIComponent(subData);
+      }).join('&');
+    } else {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+    }
+  }).join('&');
+}
+
 
 class Updater {
 
@@ -77,9 +89,13 @@ class Updater {
             appPath: '',
             appPathFolder: '',
             requestOptions: {},
-			logFile: 'updater-log.txt',
+			//logFile: 'updater-log.txt',
+			logFile: false,
             callback: false,
-            update: {}			
+            update: {},
+			totalUpdated: 0,
+			myUpdates: {},
+			myArchives: {}
         }
 		/*
 		update': {
@@ -126,25 +142,6 @@ class Updater {
             ( error != 'undefined' ? errors[error] : false ),
             this.setup.update.last)
     }
-
-	/**
-     * Detect modules
-     * */
-	detect() { 
-		let that = this;
-		_walkSync(this.setup.appPathFolder, function(filePath, stat) {
-			that.log(filePath);
-			var fileName = _getFilename(filePath);
-			that.log(fileName);
-			that.log(fileName.substring(0, fileName.lastIndexOf('.')));
-			var extension = filePath.substring(filePath.lastIndexOf('.')+1);
-			if (extension == 'asar') {
-				// @todo push into a collection
-				
-				//require(filePath);
-			}
-		});
-	}
 	
 	/**
      * Make the check for the update
@@ -169,10 +166,24 @@ class Updater {
 
         return _fetchJson(this.setup.api + '?version=' + currentVersion + '&v=' + Date.now())
             .then(result => {
-                that.log('Update available: ' + result.version)
-                that.setup.update = result
-                // Ask user for confirmation
-                that.end()
+				// If the "last" property is not defined
+                if(!result.last){
+                     throw false;
+                }
+						
+				// Update available
+				if(result.source){
+					that.log('Update available: ' + result.last);
+					// Store the response
+					that.setup.update = result;
+					// Ask user for confirmation
+					that.end();
+				} else {
+					that.log('No updates available');
+                    that.end(2);
+
+                    return false;
+				}
             })
     }
 
@@ -249,6 +260,279 @@ class Updater {
             // Failure
             that.end(6);
         }
+    }
+	
+	/**
+     * Detect modules
+     * */
+	modular_detect() { 
+		let that = this;
+		
+		_walkSync(that.setup.appPathFolder, function(filePath, stat) {
+			that.log(filePath);
+			let fileName = _getFilename(filePath);			
+			let extension = filePath.substring(filePath.lastIndexOf('.')+1);
+			if (extension == 'asar' && fileName != 'electron.asar') {
+				let archiveName = fileName.substring(0, fileName.lastIndexOf('.'));
+				that.log(archiveName);
+	
+				// Get the current version
+				/*
+				let packageInfo = fs.readFileSync(that.setup.appPathFolder + fileName + '/package.json', "utf8");
+				packageInfo = JSON.parse(packageInfo);				
+				let archiveVersion = packageInfo.version;
+				
+				that.setup.myArchives[archiveName] = archiveVersion;
+				*/
+				
+				// this causes asar file lock
+				let packageInfo = require(that.setup.appPathFolder + fileName + '/package.json');
+				let archiveVersion = packageInfo.version;
+			
+				that.setup.myArchives[archiveName] = archiveVersion;
+				
+			}
+		});
+		
+		
+		//that.setup.myArchives["app-pda"] = "1.0.1";
+		//that.setup.myArchives["app"] = "1.0.0";
+
+		that.log(that.setup.myArchives);
+	}
+	
+	/**
+     * Make the check for the update
+     * */
+    modular_check(callback) {
+        if (callback) {
+            this.setup.callback = callback;
+        }
+
+        let that = this;
+		
+		that.modular_detect();
+		
+		// If the version property not specified
+        if (!that.setup.myArchives) {
+            that.log('The "version" property not specified inside the application package.json');
+            that.end(0);
+            return false;
+        }
+
+		let params = _getAsUriParameters(that.setup.myArchives);
+		that.log(params);
+
+        return _fetchJson(this.setup.api + '?' + params + '&v=' + Date.now())
+            .then(result => {
+				// If the "last" property is not defined
+				/*
+                if(!result.last){
+                     throw false;
+                }
+				*/
+				let hasUpdate = false;
+						
+				Object.keys(result).forEach(function(key) {
+					//console.log(key, result[key]);
+					
+					// Update available
+					if (result[key].source) {
+						that.log(key+' - Update available : ' + result[key].last);
+						// Store the response
+						that.setup.myUpdates[key] = result[key];
+				
+						hasUpdate = true;		
+					}
+					
+				});
+
+				if (hasUpdate) {
+					// Ask user for confirmation
+					console.log(that.setup.myUpdates);
+					that.end();
+				} else {
+					that.log('No updates available');
+                    that.end(2);
+
+                    return false;
+				}						
+				
+            })
+    }
+		
+	/**
+     * Download the update files
+     * */
+    modular_download(callback) {
+        if (callback) {
+            this.setup.callback = callback;
+        }
+
+        let that = this;
+		let code = 0;
+		
+		if (that.setup.myUpdates) {
+			that.setup.totalUpdated = Object.keys(that.setup.myUpdates).length;
+			that.log('Total Updates:' + that.setup.totalUpdated);
+			
+			Object.keys(that.setup.myUpdates).forEach(function(key) {				
+				let currentUpdate = that.setup.myUpdates[key];
+				//console.log(key, currentUpdate);
+								
+				let url = currentUpdate.source, fileName = 'update-'+key+'.maj';
+				if (currentUpdate.source_zip) {
+					url = currentUpdate.source_zip;
+					fileName = 'update-'+key+'.zip';
+				}
+				
+				let dist = path.join(that.setup.appPathFolder, fileName);
+				
+				that.log(key+' - Downloading:' + url);
+
+				_fetchFile(url + '?v=' + Date.now(), dist)
+					//.progress(p => this.emit('progress', task, p))
+					.then(filename => {
+						that.setup.myUpdates[key].file = dist;
+						that.log(key+' - Update downloaded: ' + dist);						
+						code = that.modular_apply(key);
+						that.setup.totalUpdated--;
+						if (that.setup.totalUpdated < 1) {
+							// Success
+							that.log('End of all updates:' + that.setup.totalUpdated);		
+							that.end();
+						}
+				});
+			
+			});
+			
+			/*
+			if (code > 0) {
+				// Failure
+				that.end(code);
+			} else {
+				// Success
+				that.end();
+			}
+			*/
+		}		
+    }
+	
+	/**
+     * Apply the update, it simply overwrites the current files!
+     * */
+    modular_apply(key) {
+        let that = this;
+	
+        try {
+			var extension = that.setup.myUpdates[key].file.substring(that.setup.myUpdates[key].file.lastIndexOf('.')+1);
+			if (extension && extension == 'zip') {
+				that.log(key+' - Extracting the new update files.');
+
+                var zip = new Zip(that.setup.myUpdates[key].file);
+                zip.extractAllTo(that.setup.appPathFolder, true);
+				
+				that.log(key+' - New update files were extracted.');
+				
+				let urlToDelete = that.setup.appPathFolder + 'update-'+key+'.zip';
+				fileSystem.unlinkSync(urlToDelete);
+				that.log(key+" - Zip deleted successfully.");
+			} 
+		} catch (error) {
+            that.log(key+' - Extraction error: ' + error);
+
+            // Failure
+            return 6;
+        }				
+			
+		try {
+			let urlToDelete = that.setup.appPathFolder + key + '.asar'; 
+			console.log('urlToDelete '+urlToDelete);
+			//this.setup.appPath.slice(0, -1)
+			fileSystem.unlinkSync(urlToDelete);
+			that.log(key+" - Asar deleted successfully.");
+					 /*
+					fileSystem.unlink(urlToDelete, function (err) {
+						if (err) {
+							return console.error(err);
+						}
+						that.log(key+" - Asar deleted successfully.");
+					})
+					*/
+			that.log(key+' - Renaming the new update files.');
+				
+			fileSystem.renameSync(that.setup.myUpdates[key].file, that.setup.appPathFolder + key + '.asar');
+				
+			that.log(key+" - Update applied.");
+					/*
+					fileSystem.rename(that.setup.myUpdates[key].file, that.setup.appPathFolder + key + '.asar', function (err) {
+						if (err) {
+							that.force_copy(key);
+							//return console.error(err);
+						}
+						that.log(key+" - Update applied.");
+					})
+					*/					
+			
+		} catch (error) {
+			that.log(key+' - Delete/Rename error: ' + error);
+			
+			that.force_copy(key);
+			// Failure
+			//return 6;
+		}
+				
+			
+		that.log(key+' - End of update.');
+			
+        // Success
+		return 0;
+			     
+    }
+	
+	// app.asar is always EBUSY on Windows or some other modules are locked with modular_detect checking package version, 
+	// so we need to try another way of replacing it. With xcopy, we dont need to call it after the main Electron
+    // process has quit. 
+    force_copy(key) {
+		let that = this;
+		
+		key = key || 'app';
+		that.log('force_copy key='+key);
+		 
+        let updateAsar = this.setup.appPathFolder + 'update-' + key + '.maj';
+        let appAsar = this.setup.appPathFolder + key + '.asar';
+        let winArgs = "";
+          
+        that.log("Checking for " + updateAsar);
+        try {
+            that.log("Going to shell out to move: " + updateAsar + " to: " + appAsar);
+
+            if (process.platform === 'win32') {
+              // so ugly - this opens a dos window, which waits for 5 seconds (by which time the app.asar is not EBUSY)
+              // and then does the move - really needs to be invisible, but will do ATM
+			  
+              //winArgs = 'xcopy /V /Y "'+updateAsar+'" "'+appAsar+'"';
+			  //winArgs = 'xcopy /V /Y "'+updateAsar+'" "'+appAsar+'" && del /Q "'+updateAsar+'"';
+			  winArgs = 'xcopy /V /Y "'+updateAsar+'" "'+appAsar+'" && if errorlevel 0 (del /Q "'+updateAsar+'")';
+			  //winArgs = this.setup.appPathFolder+'update.bat "'+updateAsar+'" "'+appAsar+'"';
+			  
+			  //winArgs = 'move /y "'+updateAsar+'" "'+appAsar+'"';
+			  //winArgs = `timeout /t 5 > nul && move /y ${JSON.stringify(updateAsar)} ${JSON.stringify(appAsar)}`
+              that.log(winArgs);
+              const childpid = spawn('cmd', ['/s', '/c', '"'+winArgs+'"'], {detached: true, windowsVerbatimArguments: true, stdio: 'ignore'});
+			  //child.spawn('cmd', ['/s', '/c', '"' + winArgs + '"'], {detached: true, windowsVerbatimArguments: true, stdio: 'ignore'});
+			  childpid.unref; // let the child live on
+            
+            } else {
+              //child.spawn('bash', ['-c', ['cd ' + JSON.stringify(this.setup.appPathFolder), 'mv -f update.asar app.asar'].join(' && ')], {detached: true});
+            }
+            //child.unref; // let the child live on
+            
+            // child.exec(`${process.platform==='win32'?'move /y':'mv'} "${updateAsar}" "${appAsar}"`)
+        } catch(error) {
+            that.log("Shelling out to move failed: " + error);
+        }
+
     }
 }
 
